1898867dc15f06f4135fa052100c9fc708bb1ab9
Higor Dutra <higor.dutra@hotmail.com>
2023-03-14 18:59:06 -0300

    Implementação de Logs e Documentação com Swagger

--- DevEncurtaURL.API/Controllers/ShortenedLinksController.cs
+++ DevEncurtaURL.API/Controllers/ShortenedLinksController.cs
@@ -2,6 +2,7 @@
 using DevEncurtaURL.API.Models;
 using DevEncurtaURL.API.Persistence;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
 namespace DevEncurtaURL.API.Controllers
 {
@@ -14,11 +15,24 @@ namespace DevEncurtaURL.API.Controllers
         {
             _context = context;
         }
+
+        /// <summary>
+        /// Obter uma listagem dos Ids
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public IActionResult Get()
         {
+            Log.Information("GetAll is called!");
+
             return Ok(_context.Links);
         }
+
+        /// <summary>
+        /// Obter um Id específico
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
@@ -29,7 +43,17 @@ namespace DevEncurtaURL.API.Controllers
             return Ok(link);
         }
 
+        /// <summary>
+        /// Cadastrar um Link encurtado
+        /// </summary>
+        /// <remarks>
+        /// { "title": "ultimo-repositorio Git", "destinationLink": "https://github.com/higordutraa/DevEncurtaUrl" }
+        /// </remarks>
+        /// <param name="model">Dados de link</param>
+        /// <returns>Objetos recém-criado</returns>
+        /// <response code = "201">Sucesso!</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)] 
         public IActionResult Post(AddOrUpdateShortenedLinkModel model)
         {
             var link = new ShortenedCustomLink(model.Title, model.DestinationLink);
@@ -41,6 +65,12 @@ namespace DevEncurtaURL.API.Controllers
             return CreatedAtAction(nameof(GetById), new { id = link.Id }, link);
         }
 
+        /// <summary>
+        /// Atualizar um Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
         [HttpPut("{id}")]
         public IActionResult Put(int id, AddOrUpdateShortenedLinkModel model)
         {
@@ -57,6 +87,11 @@ namespace DevEncurtaURL.API.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Deletar um Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
